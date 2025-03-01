fd887b319b561789a5be644989cb419790d9f158
Wanyonyi <g.nanguti@gmail.com>
2025-03-01 05:17:55 +0300

    Backend domain configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ["images.unsplash.com", "localhost", "127.0.0.1"],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "blvdguide.swafifilms.com",
+      },
+    ],
+    domains: [
+      "blvdguide.swafifilms.com",
+      "images.unsplash.com",
+      "localhost",
+      "127.0.0.1",
+    ],
   },
 };
 
